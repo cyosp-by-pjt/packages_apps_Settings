ec1f34aa9c8b9b24f2d48498eb57b69b45012e4a
pjt <polyak.tamas@gmail.com>
2015-11-08 23:26:56 +0100

    cyosp version added to About Phone

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -82,6 +82,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+    private static final String KEY_CYOSP_VERSION = "cyosp_version";
+    private static final String KEY_CYOSP_PROP = "ro.cyosp.version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -129,6 +131,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setValueSummary(KEY_CYOSP_VERSION, KEY_CYOSP_PROP);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
@@ -520,4 +523,3 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         };
 
 }
-
